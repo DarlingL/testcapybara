ac2ba1fef3a3d1d7f9fb01ae5377b7d52dca588a
Felipe Simões <felipe_lima_simoes@hotmail.com>
2017-08-17 15:44:02 -0300

    click em buscar por xpath

--- cucumber/features/step_definitions/taskList_steps.rb
+++ cucumber/features/step_definitions/taskList_steps.rb
@@ -7,11 +7,12 @@ Dado(/^digito na busca Cruzeiro Globo Esporte$/) do
 end
 
 Dado(/^Clico em Buscar$/) do
-  click_on "Pesquisa Google"
+  find(:id, 'hplogo').click
+  find(:xpath, '//*[@id="tsf"]/div[2]/div[3]/center/input[1]').click
 end
 
 Dado(/^Seleciono o primeiro resulto$/) do
-click_on "cruzeiro | GloboEsporte.com"
+  click_on "cruzeiro | GloboEsporte.com"
 end
 
 Entao(/^estarei na pagina do melhor time do mundo$/) do
